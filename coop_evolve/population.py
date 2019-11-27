36c8a831f7b1be4a317449ec129dfa9ee4ffe9bf
Robert Olendorf <drolendorf@gmail.com>
2019-11-27 16:42:32 +0000

    reproduction done

--- coop_evolve/population.py
+++ coop_evolve/population.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import copy
+import numpy
 import random
 
 from app_settings import AppSettings
@@ -35,7 +37,7 @@ class Population:
         
         Parameters
         ---------
-        interactions: integer, default = 1
+        interactions: Integer, default = 1
             The number of interactions per agent. If interactions = 2, and there are
             10 agents in the population, then there are 20 interactions and each agent
             is expected to take part in 40 interactions. 
@@ -44,7 +46,7 @@ class Population:
         
         for i in range(self.width):
             for j in range(self.height):
-                for k in range(interactions * self.subpop_size):
+                for _ in range(interactions * self.subpop_size):
                     index1 = random.randint(0, (self.subpop_size - 1))
                     index2 = random.randint(0, (self.subpop_size - 1))
                     while index1 == index2:
@@ -52,7 +54,63 @@ class Population:
                     agent1 = self.population[i][j][index1]
                     agent2 = self.population[i][j][index2]
                     Agent.interact(agent1, agent2)
+
+    def reproduce(self, fecundity = 1, relative_fitnesses = True):
+        """
+        Agents reproduce based on their relative fitness.
+        
+        The probability that an agent reproduces is based on its fitness divided by the subpopulation total fitness 
+        times fecundity * subpopulation size. So if there are 10 agents, fecundity of one, and the relative 
+        fitness is 0.25, the agent will on average have 2.5 offspring.fitness
+        
+        Parameters
+        ----------
+        fecundity: Integer, default = 1
+            The average number of offspring per agent.
+            
+        relative_fitnesses: Boolean, default = True
+            If true agents relative fitness is used. Agents reproduce proportianal to their relative fitness (fitness)/(sum(subpop_fitness))
+            If false, absolute fitness, each agent has the probability of reproducing fitness/max_possible_fitness. Relative fitness reproduction results in 
+            equal reproduction across subpopulations. Absolute fitness results in subpopulation reproduction being relative to the mean fitness 
+            in the subpopulation
+            
+        """
+        
+        if(relative_fitnesses):
+            self.__reproduce_with_relative_fitness(fecundity)
+        else:
+            self.__reproduce_with_absolute_fitness(fecundity)
+            
+    def __reproduce_with_relative_fitness(self, fecundity):
+        
+        for i in range(self.width):
+            for j in range(self.height):
+                relative_fitnesses = []
+                for k in range(self.subpop_size):
+                    relative_fitnesses.append(self.population[i][j][k].fitness())
+                relative_fitnesses = numpy.cumsum([(i/sum(relative_fitnesses)) for i in relative_fitnesses]).tolist()
+                for _ in range(fecundity * self.subpop_size):
+                    index = 0
+                    rand = random.random()
+                    while rand > relative_fitnesses[index]:
+                        index += 1
+                    self.population[i][j].append(copy.deepcopy(self.population[i][j][index]))
+                    
+    def __reproduce_with_absolute_fitness(self, fecundity):
+        cfg = AppSettings()
+        max_payoff = max(cfg.payoffs.values())
+        for i in range(self.width):
+            for j in range(self.height):
+                popsize = len(self.population[i][j])
+                for _ in range(fecundity):
+                    for k in range(popsize):
+                        if random.random() <= self.population[i][j][k].fitness()/max_payoff:
+                            self.population[i][j].append(copy.deepcopy(self.population[i][j][k]))
+                
+                    
+                    
                         
+                    
             
     def __getitem__(self, key):
         """
@@ -69,20 +127,20 @@ class Population:
         """
         return self.population[key]
         
-    def __setitem__(self, key, value): # pragma: no cover
-        """
-        Allows items, slices etc to be assigned (or reassigned). For example
-        `pop[0] = Agent()`
+    # def __setitem__(self, key, value): # pragma: no cover
+    #     """
+    #     Allows items, slices etc to be assigned (or reassigned). For example
+    #     `pop[0] = Agent()`
         
-        Parameters
-        __________
-        key: Any list indexing or slicing parameters
+    #     Parameters
+    #     __________
+    #     key: Any list indexing or slicing parameters
         
-        value: Agent|[Agents]
-            An agent or list of agents.
+    #     value: Agent|[Agents]
+    #         An agent or list of agents.
             
-        """
-        self.population[key] = value
+    #     """
+    #     self.population[key] = value
         
             
 

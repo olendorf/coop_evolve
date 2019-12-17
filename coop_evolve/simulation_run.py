f850077115d902519bcad4d8cb1360b7dd7cc7e6
Ubuntu <ubuntu@ip-172-31-19-235.ec2.internal>
2019-12-17 18:33:16 +0000

    simulatoin run sketched out

--- coop_evolve/simulation_run.py
+++ coop_evolve/simulation_run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from coop_evolve.population import Population
+
 class SimulationRun:
     
     def __init__(self, generations = 10000,
@@ -9,7 +11,8 @@ class SimulationRun:
                        subpop_size = 100,
                        relative_fitnesses = True,
                        migration_survival = 0.1,
-                       migration_distance = 1
+                       migration_distance = 1,
+                       initial_sequence = None
                  ):
         self.generations = generations
         self.width = width
@@ -18,5 +21,27 @@ class SimulationRun:
         self.relative_fitnesses = relative_fitnesses
         self.migration_survival = migration_survival
         self.migration_distance = migration_distance
+        self.initial_sequence = initial_sequence
         
+        self.data = [{}]
         
+        self.population = Population(
+            width = self.width, 
+            length = self.length, 
+            subpop_size = self.subpop_size,
+            sequence = self.initial_sequence
+        )
+                
+        
+    def run(self):
+        
+        for _ in range(self.generations):
+            data = {}
+            self.population.play_game()
+            self.population.reproduce()
+            self.population.migrate()
+            self.population.cull()
+            self.data.append(data)
+            
+            
+            
